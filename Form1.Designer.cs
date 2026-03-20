880ef876590747e9fdad14350b3c6e12df8e8cc4
명원 이 <myeongwon4743@suwon.ac.kr>
2026-03-20 14:57:40 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace EchoMessenger
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            labelCount = new Label();
+            button1 = new Button();
+            listBox1 = new ListBox();
+            textBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // labelCount
+            // 
+            labelCount.AutoSize = true;
+            labelCount.Location = new Point(26, 18);
+            labelCount.Name = "labelCount";
+            labelCount.Size = new Size(94, 15);
+            labelCount.TabIndex = 0;
+            labelCount.Text = "Echo Messenger";
+            labelCount.Click += label1_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(674, 381);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 1;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(26, 51);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(723, 304);
+            listBox1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(26, 381);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(637, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(788, 453);
+            Controls.Add(textBox1);
+            Controls.Add(listBox1);
+            Controls.Add(button1);
+            Controls.Add(labelCount);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label labelCount;
+        private Button button1;
+        private ListBox listBox1;
+        private TextBox textBox1;
+    }
+}
